ac49d3e698107952220313eb89d871bea2f8f37b
Patrick Gray <clifgray@Patricks-MacBook-Pro-3.local>
2017-08-08 16:27:37 -0400

    manual controller minimization

--- manual_controllers/calboard_controller.py
+++ manual_controllers/calboard_controller.py
@@ -1,54 +1,11 @@
-'''
-Created on Mar, 03 2017
-
-@author: pgray
-
-@description: desktop calibration app for Girguis ISMS
-'''
-
-import Tkinter as tk
-import ttk
 import serial
 import time
-from collections import deque
-import logging
-import math
 
-### Logging Config ###
+serial_list = ['COM28']
 
-logger = logging.getLogger(__name__)
-#handler = logging.FileHandler('calibration.log')
-handler2 = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s:\t%(message)s')
-#handler.setFormatter(formatter)
-handler2.setFormatter(formatter)
-#ogger.addHandler(handler)
-logger.addHandler(handler2)
-logger.setLevel(logging.DEBUG)
-
-
-#serial_list = ['/dev/tty.usbserial-A800dars', '/dev/tty.usbserial', '/dev/tty.usbserial-AE01I93I', '/dev/tty.usbmodem14111']
-serial_list = ['', '', '', 'COM28', '']
-serial_check_list = [None] * len(serial_list)
-
-def check_serial():
-	print("************ checking serial")
-	for index, serial_port in enumerate(serial_list):
-		try:
-			ser = serial.Serial(serial_port)	  	# open serial port
-			serial_check_list[index] = ser.name 	# check which port was really used
-			ser.close()             				# close port
-			logger.debug("Acquired serial connection.")
-		except Exception as e:
-			template = str(type(e).__name__) + " occured. Arguments:" + str(e.args)
-			logger.error(template)
-	return serial_check_list
-
-
-# controller for the arduino that controls the circuit board that controls the calibration board
 class CalBoard_Controller():
 	def __init__(self):
-		self.ser = serial.Serial(serial_list[3], 9600, timeout=3)
+		self.ser = serial.Serial(serial_list[0], 9600, timeout=3)
 		time.sleep(1)
 		logger.info("Starting Calibration Board Controller")
 		logger.debug("Connected over serial at " + str(self.ser.name))
@@ -148,62 +105,34 @@ class CalBoard_Controller():
 			final_pressures.append(pressure)
 
 		return(final_pressures)
+	
 
 
-def check_health():
-	cc = CalBoard_Controller()
-	if (cc.is_healthy() == True):
-		# calboard controller is healthy and can continue
-		logger.debug("Calboard Controller is healthy, moving forward.\n\n")
-		return(True)
+cc = CalBoard_Controller()
+while(True):
+	user_input = input("Enter user input:")
+	print(user_input)
+	if user_input == 1:
+		cc.normal_operation("v9")
+	elif user_input == 2:
+		cc.normal_operation("v8")
+	elif user_input == 3:
+		cc.seawater_in()
+	elif user_input == 4:
+		cc.di_water_in()
+	elif user_input == 5:
+		cc.release_press()
+	elif user_input == 6:
+		cc.flush_off()
+	elif user_input == 7:
+		print(cc.read_press())
+	elif user_input == 8:
+		cc.reset()
+	elif user_input == -1:
+		cc.ser.close()
+		break
 	else:
-		# something is wrong and need to trip a pause and alarm and wait for user input
-		err_msg = "Calboard is unhealthy, stopping calibration.\n\n"
-		logger.error(err_msg)
-		return(False)
-
-def run_valves():
-	cc = CalBoard_Controller()
-	#cc.is_healthy()
-	
-	#cc.state_one()
-	#time.sleep(1)
-	cc.state_two()
-	time.sleep(1)
-	cc.flush_on()
-	#time.sleep(3)
-	#cc.flush_off()
-	
-	
-
-if any(check_serial()):
-	print("**************passed check serial")
-	print(serial_check_list)
-	if check_health():
-		print("passed health check")
-		cc = CalBoard_Controller()
-		while(True):
-			user_input = input("Enter user input:")
-			print(user_input)
-			if user_input == 1:
-				cc.normal_operation("v9")
-			elif user_input == 2:
-				cc.normal_operation("v8")
-			elif user_input == 3:
-				cc.seawater_in()
-			elif user_input == 4:
-				cc.di_water_in()
-			elif user_input == 5:
-				cc.release_press()
-			elif user_input == 6:
-				cc.flush_off()
-			elif user_input == 7:
-				print(cc.read_press())
-			elif user_input == -1:
-				cc.ser.close()
-				break
-			else:
-				print("didn't match option")
+		print("didn't match option")
 
 
 
--- manual_controllers/mfc_controller.py
+++ manual_controllers/mfc_controller.py
@@ -1,55 +1,15 @@
-'''
-Created on Mar, 03 2017
-
-@author: pgray
-
-@description: desktop calibration app for Girguis ISMS
-'''
-
-import Tkinter as tk
-import ttk
 import serial
 import time
-from collections import deque
-import logging
-import math
-import binascii
 
-### Logging Config ###
-showtime = time.strftime("%Y_%m_%d-%H_%M_%S", time.gmtime())
-logger = logging.getLogger(__name__)
-#handler = logging.FileHandler('_calibration' + showtime + '.log')
-handler2 = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s:\t%(message)s')
-#handler.setFormatter(formatter)
-handler2.setFormatter(formatter)
-#logger.addHandler(handler)
-logger.addHandler(handler2)
-logger.setLevel(logging.DEBUG)
 
 from calcCRC import calcCRC
 
-#serial_list = ['/dev/tty.usbserial-A800dars', '/dev/tty.usbserial', '/dev/tty.usbserial-AE01I93I', '/dev/tty.usbmodem1411', '/dev/tty.usbserial']
-serial_list = ['', '', '', '', 'COM32']
-serial_check_list = [None] * len(serial_list)
-
-def check_serial():
-	print("************ checking serial")
-	for index, serial_port in enumerate(serial_list):
-		try:
-			ser = serial.Serial(serial_port)	  	# open serial port
-			serial_check_list[index] = ser.name 	# check which port was really used
-			ser.close()             				# close port
-			logger.debug("Acquired serial connection.")
-		except Exception as e:
-			template = str(type(e).__name__) + " occured. Arguments:" + str(e.args)
-			logger.error(template)
-	return serial_check_list
+serial_list = ['COM32']
 
 
 class MFC_Controller_One():
 	def __init__(self):
-		self.ser = serial.Serial(serial_list[4], 9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3)
+		self.ser = serial.Serial(serial_list[0], 9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3)
 		time.sleep(1)
 		logger.info("Starting MFC Controller")
 		logger.debug("Connected over serial at " + str(self.ser.name))
@@ -143,7 +103,6 @@ def run_cmds():
 	#mc_1.set_setpoint(150)
 	mc_1.read_flow()
 	
-#if check_health():
 run_cmds()
 
 
